ed4a03edc6a7628a9bcb70c1276c1f5f95859000
KietNguyen <kietntlongan@gmail.com>
2017-10-04 23:10:48 +0700

    First created

---
+++ README.md
@@ -0,0 +1,3 @@
+# sails-react-first-version
+
+a [Sails](http://sailsjs.org) application
---
+++ views/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // can even use ES2015
+//export default () => {
+//  <h1>Hello Sails + React</h1>
+//}
+
+class HomeComponent extends React.Component {
+  render() {
+    return <h1>Hello Sails + React</h1>;
+  }
+}
+
+export default HomeComponent;
